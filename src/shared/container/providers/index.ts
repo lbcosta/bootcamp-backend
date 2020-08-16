dbcee6e6a59021dfb97afec7702198e9eef7f8e6
Leonardo Costa <lbcosta.dev@gmail.com>
2020-08-15 22:00:02 -0300

    3.2 - Perfil do usuário | Emails em desenvolvimento

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProdiver from './MailProvider/implementations/EtherealMailProdiver';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProdiver(),
 );
